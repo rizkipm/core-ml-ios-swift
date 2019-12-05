142307181cab52cf09a547887c1894561f1b1775
Rizki Syaputra <mr.rizkisyaputra@gmail.com>
2019-12-05 13:34:15 +0700

    add

--- CoreML/CoreML/ViewController.swift
+++ CoreML/CoreML/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
 
     @IBOutlet weak var labelText: UILabel!
     @IBOutlet weak var imgGambar: UIImageView!
+    
+    var model : Inceptionv3!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        model = Inceptionv3()
     }
 
     @IBAction func btnCamera(_ sender: Any) {
@@ -41,6 +47,51 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
 extension ViewController : UIImagePickerControllerDelegate{
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        labelText.text = "Analyzing Image ..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGImageCompatibilityKey : kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else{
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: (Int(newImage.size.height)), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0) )
+        imgGambar.image = newImage
+        
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else{
+            return
+        }
+        
+        labelText.text  = "I think this is a \(prediction.classlabel)"
+        
+        
     }
 }
 
